90959d343a1d655c70bff201e0ad020dd6431cb4
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-03-22 15:20:01 +0100

    add all images and fonts

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Footer from "./Footer/Footer";
+
 import Main from "./Main/Main";
 import Header from "./Header/Header";
-
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
