5892f0264e7d8ccb2de6e1482969267e00fe0c80
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-03-22 14:53:57 +0100

    add сreate basic markup

---
+++ src/Components/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from "./Footer/Footer";
+import Main from "./Main/Main";
+import Header from "./Header/Header";
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+
+
+    </div>
+  );
+}
+
+export default App;
