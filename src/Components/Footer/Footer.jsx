8a834b4d5e9c04c9394f8e8f3a00f080dc3e5e31
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-03-23 06:44:57 +0100

    add Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import './Footer.css'
+import logo from '../../assets/img/footer/logo-footer.svg'
 
 const Footer = () => {
     return(
-        <footer>
-
-
+        <footer className="footer">
+            <div className="footer__content">
+            <div>
+                <a href="#"><img src={logo}/></a>
+            </div>
+            <p>Terms of Service Privacy  Responsible Disclosure Copyright © 2017-2023, Recidiviz. All Rights Reserved.</p>
+        </div>
         </footer>
     )
 
