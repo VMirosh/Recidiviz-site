ec8c47d1071bfe9966aa1be90eeeb50d046ef78c
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-03-25 02:05:44 +0100

    add data through props header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,46 +1,72 @@
 import './Header.css'
-import logo from '../../assets/img/header/header-logo.svg'
+// import logo from '../../assets/img/header/header-logo.svg'
+
+const Headerlink = (props) => {
+    return(
+        <li><a href="#">{props.link}</a></li>
+    )
+}
+const Headerlogin = (props) =>{
+    return(
+        <li><a href="#">{props.data}</a></li>
+    )
+}
+const BlockLink =(props) =>{
+    return(
+        <a href="#">{props.data}</a>
+    )
+};
+
+const BottomItem = (props) =>{
+    return(
+        <li>{props.data}</li>
+
+    )
+};
 
 
+const Header = (props) => {
 
-const Header = () => {
     return(
         <header className="header">
+
             <div className="header__link">
-                <a href="#">Watch our new TED Talk →</a>
+                <BlockLink data={props.blockLink}></BlockLink>
             </div>
+
             <div className="container">
             <div className="header__top">
             <nav className="nav">
                 <div className="nav__logo">
-                    <a href="#"><img src={logo} alt="logo" className="logo"/></a>
+                    <a href="#"><img src={props.top.logo} alt="logo" className="logo"/></a>
                 </div>
                 <ul className="nav__list">
-                    <li><a href="#">States</a></li>
-                    <li><a href="#">Tools</a></li>
-                    <li><a href="#">People</a></li>
-                    <li><a href="#">Learn More</a></li>
-                    <li><a href="#">Donate</a></li>
-                    <li className="nav__login">
-                        <a href="#">Log In</a>
-                    </li>
+                    {props.top.links.map(link => <Headerlink link = {link}/>)}
+                </ul>
+                <ul className="nav__login">
+                    {props.top.login.map(data => <Headerlogin data= {data}/>)}
                 </ul>
             </nav>
             </div>
+
                 <div className="header__content">
-                    <h1 className='content-title'> Every person who safely transitions back to the community is a step toward a better society.</h1>
-                    <p className='content-aside'>Criminal justice leaders use Recidiviz to get a real-time picture of their system, diagnose issues, and drive meaningful changes.</p>
-                    <div className="btn">See our work in action</div>
+                    <h1 className='content-title'>
+                        {props.content.title}
+                    </h1>
+                    <p className='content-aside'>
+                        {props.content.description}
+                    </p>
+                    <div className="btn">
+                        {props.content.button}
+                    </div>
                 </div>
+
                 <div className="header__bottom">
-                    <h4 className='bottom-title'>Trusted in:</h4>
+                    <h4 className='bottom-title'>
+                        {props.bottom.title}
+                    </h4>
                     <ul className="header__bottom-trusted">
-                        <li>Idaho</li>
-                        <li>Maine</li>
-                        <li>Missouri</li>
-                        <li>North Dakota</li>
-                        <li>Pennsylvania</li>
-                        <li>Tennessee</li>
+                        {props.bottom.cities.map(data => <BottomItem data = {data}/>)}
                     </ul>
 
                 </div>
